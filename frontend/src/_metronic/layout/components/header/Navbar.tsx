2e03e5fcf5565c41a36efc53bde44bd17aac9a0c
knightzxc12345 <knightzxc12345@gmail.com>
2025-01-20 15:30:25 +0800

    remove search

--- frontend/src/_metronic/layout/components/header/Navbar.tsx
+++ frontend/src/_metronic/layout/components/header/Navbar.tsx
@@ -1,11 +1,9 @@
 import clsx from 'clsx'
 import {KTIcon, toAbsoluteUrl} from '../../../helpers'
-import {HeaderUserMenu, Search} from '../../../partials'
+import {HeaderUserMenu} from '../../../partials'
 import {useLayout} from '../../core'
 
 const itemClass = 'ms-1 ms-md-4'
-const btnClass =
-  'btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px'
 const userAvatarClass = 'symbol-35px'
 const btnIconClass = 'fs-2'
 
@@ -13,10 +11,6 @@ const Navbar = () => {
   const {config} = useLayout()
   return (
     <div className='app-navbar flex-shrink-0'>
-      <div className={clsx('app-navbar-item align-items-stretch', itemClass)}>
-        <Search />
-      </div>
-
       <div className={clsx('app-navbar-item', itemClass)}>
         <div
           className={clsx('cursor-pointer symbol', userAvatarClass)}
--- frontend/src/_metronic/partials/index.ts
+++ frontend/src/_metronic/partials/index.ts
@@ -1,6 +1,4 @@
 export * from './layout/quick-links/QuicLinks'
-export * from './layout/search/Search'
-export * from './layout/search/SearchInner'
 export * from './layout/SearchModal'
 export * from './layout/InboxCompose'
 export * from './layout/header-menus/HeaderUserMenu'
@@ -13,9 +11,7 @@ export * from './content/dropdown/Dropdown2'
 
 // editors
 export * from './content/editor/EditorToolbar'
-
 export * from './content/code-highlight/CodeBlock'
-
 export * from './content/portal/Portal'
 
 // modals
