3c3415710cfcd49ded542cdf54313d2ff6f738c0
knightzxc12345 <knightzxc12345@gmail.com>
2025-01-21 14:46:45 +0800

    remove menutest

--- frontend/src/app/routing/PrivateRoutes.tsx
+++ frontend/src/app/routing/PrivateRoutes.tsx
@@ -3,7 +3,6 @@ import {Route, Routes, Navigate} from 'react-router-dom'
 import {MasterLayout} from '../../_metronic/layout/MasterLayout'
 import TopBarProgress from 'react-topbar-progress-indicator'
 import {DashboardWrapper} from '../pages/dashboard/DashboardWrapper'
-import {MenuTestPage} from '../pages/MenuTestPage'
 import {getCSSVariableValue} from '../../_metronic/assets/ts/_utils'
 import {WithChildren} from '../../_metronic/helpers'
 
@@ -30,7 +29,6 @@ const PrivateRoutes = () => {
         />
         {/* Pages */}
         <Route path='dashboard' element={<DashboardWrapper />} />
-        <Route path='menu-test' element={<MenuTestPage />} />
         {/* Lazy Modules */}
         <Route
           path='crafted/pages/profile/*'
