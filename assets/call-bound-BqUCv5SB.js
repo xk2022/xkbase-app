0decc60d1207f1d2b5bc740f2984f337f0339f4c
Louis Chen <imomi329@gmail.com>
2026-01-08 18:33:51 +0800

    Updates

---
+++ assets/call-bound-BqUCv5SB.js
@@ -0,0 +1,1 @@
+import{g as c}from"./get-intrinsic-CU0VT1nQ.js";import{c as a}from"./call-bind-apply-helpers-CZTuF3Zf.js";var n=c,t=a,e=t([n("%String.prototype.indexOf%")]),f=function(i,o){var r=n(i,!!o);return typeof r=="function"&&e(i,".prototype.")>-1?t([r]):r};export{f as c};
